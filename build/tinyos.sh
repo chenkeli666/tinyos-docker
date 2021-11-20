1028ae7801af2abaced1f922da2ac6eaec728b86
osennyaya <supersduby@gmail.com>
2021-11-20 19:28:03 +0800

    fix encoding

--- build/tinyos.sh
+++ build/tinyos.sh
@@ -1,18 +1,18 @@
-#! /usr/bin/env bash   
-# www.ElectronicsPub.com   
-# TinyOS 2.1.2 Configuration Guide   
-# Here we setup the environment   
-# variables needed by the tinyos    
-# make system   
-echo "Setting up for TinyOS 2.1.2"   
-export TOSROOT=   
-export TOSDIR=   
-export MAKERULES=   
-TOSROOT="/opt/tinyos-2.1.2"   
-TOSDIR="$TOSROOT/tos"   
-CLASSPATH=$CLASSPATH:$TOSROOT/support/sdk/java   
-MAKERULES="$TOSROOT/support/make/Makerules"   
-export TOSROOT   
-export TOSDIR   
-export CLASSPATH   
+#!/usr/bin/env bash
+# www.ElectronicsPub.com
+# TinyOS 2.1.2 Configuration Guide
+# Here we setup the environment
+# variables needed by the tinyos
+# make system
+echo "Setting up for TinyOS 2.1.2"
+export TOSROOT=
+export TOSDIR=
+export MAKERULES=
+TOSROOT="/opt/tinyos-2.1.2"
+TOSDIR="$TOSROOT/tos"
+CLASSPATH="$CLASSPATH:$TOSROOT/support/sdk/java"
+MAKERULES="$TOSROOT/support/make/Makerules"
+export TOSROOT
+export TOSDIR
+export CLASSPATH
 export MAKERULES
